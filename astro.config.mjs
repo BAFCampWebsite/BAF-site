24e7da018b3e41c0ac1a3364b81f3dd2f546fda9
Jonas Vacek <jvacek@users.noreply.github.com>
2026-07-07 16:19:59 +0200

    move redirects to vercel instead, use middleware for local server forwards

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,4 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://belgian-activism-festi-camp.be',
-  redirects: {
-    '/': '/fr/',
-    '/billetterie': '/fr/billetterie',
-    '/programme': '/fr/programme',
-    '/infos': '/fr/infos',
-    '/financements': '/fr/financements',
-    '/charte': '/fr/charte',
-    '/autogestion': '/fr/autogestion',
-    '/nous-aider': '/fr/nous-aider',
-  },
 });
