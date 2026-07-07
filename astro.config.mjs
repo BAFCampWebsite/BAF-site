3f89a38018cbb7ba5714ccff56fe27e6497ce55f
Jonas Vacek <jvacek@users.noreply.github.com>
2026-07-07 14:28:53 +0200

    add redirects for non-i18n'd pages to french

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,14 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://belgian-activism-festi-camp.be',
+  redirects: {
+    '/': '/fr/',
+    '/billetterie': '/fr/billetterie',
+    '/programme': '/fr/programme',
+    '/infos': '/fr/infos',
+    '/financements': '/fr/financements',
+    '/charte': '/fr/charte',
+    '/autogestion': '/fr/autogestion',
+    '/nous-aider': '/fr/nous-aider',
+  },
 });
