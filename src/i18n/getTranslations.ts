9d2524bc021b9f620c27c37f390d4bea0017006d
Jonas Vacek <jvacek@users.noreply.github.com>
2026-07-08 16:01:29 +0200

    simplify the boilerplate for localised paths

--- src/i18n/getTranslations.ts
+++ src/i18n/getTranslations.ts
@@ -3,6 +3,9 @@ import type { Messages } from './types';
 
 type TranslationHelper<T extends Record<string, unknown> = Record<string, unknown>> = ReturnType<typeof getAstroTranslations<T>>;
 
+export const locales = ['fr', 'en', 'nl'] as const;
+export type Locale = (typeof locales)[number];
+
 export function getTranslations<N extends keyof Messages>(namespace: N): TranslationHelper<Messages[N]>;
 export function getTranslations<T extends Record<string, unknown> = Record<string, unknown>>(namespace: string): TranslationHelper<T>;
 export function getTranslations<T extends Record<string, unknown> = Record<string, unknown>>(namespace: string) {
