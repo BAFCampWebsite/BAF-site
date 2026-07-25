68539a94f65f87a4b412267e5183b9f368b4a856
Jonas Vacek <jvacek@users.noreply.github.com>
2026-07-25 16:02:32 +0200

    Add sitemap generation

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,24 +3,24 @@ import astroIntl from "astro-intl";
 import { readFileSync } from 'node:fs';
 import vercel from '@astrojs/vercel';
 
+import sitemap from "@astrojs/sitemap";
+
 export default defineConfig({
   adapter: vercel({
     webAnalytics: {
       enabled: true,
     },
   }),
-  site: 'https://belgian-activism-festi-camp.be',
-  integrations: [
-    astroIntl({
-      defaultLocale: "fr",
-      locales: ["fr", "en", "nl"],
-      messages: {
-        fr: () => JSON.parse(readFileSync("./src/i18n/fr.json", "utf-8")),
-        en: () => JSON.parse(readFileSync("./src/i18n/en.json", "utf-8")),
-        nl: () => JSON.parse(readFileSync("./src/i18n/nl.json", "utf-8")),
-      },
-    }),
-  ],
+  site: 'https://bafcamp.be',
+  integrations: [astroIntl({
+    defaultLocale: "fr",
+    locales: ["fr", "en", "nl"],
+    messages: {
+      fr: () => JSON.parse(readFileSync("./src/i18n/fr.json", "utf-8")),
+      en: () => JSON.parse(readFileSync("./src/i18n/en.json", "utf-8")),
+      nl: () => JSON.parse(readFileSync("./src/i18n/nl.json", "utf-8")),
+    },
+  }), sitemap()],
   vite: {
     plugins: [
       {
